a3a0d5e490cd8af1903b7397eec03446a3c5dddf
Suyash Bahrani <suyash.bahrani@gmail.com>
2022-01-28 15:41:13 -0600

    Level 1 security - register + login + robo3t

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", function(req,res){
     res.render("home");
 });
@@ -27,6 +34,43 @@ app.get("/login", function(req,res){
 app.get("/register", function(req,res){
     res.render("register");
 });
+
+app.post("/register", function(req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function(err){
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.render("secrets")
+        }
+    });
+
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if (err){
+            console.log(err)
+        }
+        else {
+            if (foundUser){
+                if (foundUser.password == password){
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
+
+
+
 
 
 app.listen(3000, function() { 
