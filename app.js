8119ad10e4831e9b2648f8c7b6357684f2cd0325
Suyash Bahrani <suyash.bahrani@gmail.com>
2022-01-28 17:26:47 -0600

    Level 3 - Hash Function(no need of key)

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+
+// only gets binary form that is retrievable (forward and backward)
+// const encrypt = require("mongoose-encryption");
+
+const md5 = require("md5");
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public")); 
 
@@ -25,8 +27,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-
-userSchema.plugin(encrypt , {secret: secret, encryptedFields: ["password"] });
+// with mongoose encrypt 
+// userSchema.plugin(encrypt , {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -45,7 +47,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err) {
@@ -60,7 +62,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if (err){
