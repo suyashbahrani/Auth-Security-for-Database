246373db33d93f4fd1f48d76f11ac9942263a209
Suyash Bahrani <suyash.bahrani@gmail.com>
2022-01-28 14:56:33 -0600

    ..

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ 
     extended: true
 }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 app.get("/", function(req,res){
     res.render("home");
