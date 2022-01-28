1b9a27e8a81d399f9b9cdb6b22156524773eb15a
Suyash Bahrani <suyash.bahrani@gmail.com>
2022-01-28 16:29:15 -0600

    testing gitignore & env file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -6,6 +7,8 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public")); 
 
@@ -22,7 +25,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret";
+
 userSchema.plugin(encrypt , {secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
