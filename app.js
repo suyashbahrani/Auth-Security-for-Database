d2d7fdc6a84c90d4e122479ebe58fa9bda758492
Suyash Bahrani <suyash.bahrani@gmail.com>
2022-01-28 21:52:25 -0600

    Salting and Bcrpyt Hashing fstr than hash

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const mongoose = require("mongoose");
 // only gets binary form that is retrievable (forward and backward)
 // const encrypt = require("mongoose-encryption");
 
-const md5 = require("md5");
+// const md5 = require("md5");
+
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -45,24 +49,31 @@ app.get("/register", function(req,res){
 });
 
 app.post("/register", function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save(function(err){
-        if(err) {
-            console.log(err);
-        }
-        else {
-            res.render("secrets")
-        }
-    });
 
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        console.log(hash);
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+            // password: md5(req.body.password)
+        });
+        newUser.save(function(err){
+            if(err) {
+                console.log(err);
+            }
+            else {
+                res.render("secrets")
+            }
+        });
+
+    });  
 });
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    // const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, function(err, foundUser){
         if (err){
@@ -70,10 +81,16 @@ app.post("/login", function(req, res){
         }
         else {
             if (foundUser){
-                if (foundUser.password == password){
-                    res.render("secrets");
+                // if (foundUser.password == password){
+                    bcrypt.compare(password, foundUser.password, function(err, result) {
+                        // result == true
+                        if (result === true) {
+                            res.render("secrets");
+
+                        }
+                    });
+                    
                 }
-            }
         }
     });
 });
