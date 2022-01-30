83d327b4d82a9ed1f521802dd4ff0b4c92960896
Suyash Bahrani <suyash.bahrani@gmail.com>
2022-01-30 02:57:06 -0600

    Level 6 Google Auth

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 
+
 // only gets binary form that is retrievable (forward and backward)
 // const encrypt = require("mongoose-encryption");
 
@@ -21,7 +22,8 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 
-
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 
 
@@ -49,7 +51,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleid: String
 });
 
 // with mongoose encrypt 
@@ -58,19 +61,64 @@ const userSchema = new mongoose.Schema({
 
 // hash and salt passwords
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
 
 const User = new mongoose.model("User", userSchema);
 
 // while using sessions
 // while restarting the server the cookies gets deleted
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+
+// only with local
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+
+// with all kinds
+passport.serializeUser(function(user, done){
+    done(null, user.id)
+});
+
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        done(err, user);
+    });
+    
+});
+
+
+passport.use(new GoogleStrategy({
+    // authorizationURL: 'https://www.example.com/oauth2/authorize',
+    // tokenURL: 'https://www.example.com/oauth2/token',
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+//   User.findorCreate require mongoose function or else manually create it 
+// also it does not give passwords and authenticates for us that's really awesome
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile)
+    User.findOrCreate({ googleid: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 
 app.get("/", function(req,res){
     res.render("home");
+});
+
+app.get("/auth/google",
+    passport.authenticate("google", {scope: ["profile"]} )
+);
+
+app.get("/auth/google/secrets", passport.authenticate("google", {failureRedirect: "/login"}), function(req, res) {
+    res.redirect("/secrets");
 });
 
 app.get("/login", function(req,res){
